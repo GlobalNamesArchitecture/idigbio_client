7334d254264f87179ca595e10069d00d276ee5f8
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-05-16 07:45:58 -0400

    setting up framework

--- spec/idigbio_client_spec.rb
+++ spec/idigbio_client_spec.rb
@@ -1,11 +1,6 @@
-require 'spec_helper'
-
 describe IdigbioClient do
   it 'has a version number' do
     expect(IdigbioClient::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
