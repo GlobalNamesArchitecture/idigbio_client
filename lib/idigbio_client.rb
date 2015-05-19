17e215359676f5c78ac7f9ba6f884cf99d7bc7f0
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-05-19 19:23:13 -0400

    combine all in one module

--- lib/idigbio_client.rb
+++ lib/idigbio_client.rb
@@ -2,10 +2,6 @@ require "json"
 require "logger"
 require "rest_client"
 require "idigbio_client/version"
-require "idigbio_client/search"
-require "idigbio_client/show"
-require "idigbio_client/inspect"
-require "idigbio_client/stats"
 
 # Ruby wrapper for iDigBio API
 module IdigbioClient
@@ -17,6 +13,33 @@ module IdigbioClient
   class << self
     def logger
       @logger ||= Logger.new($stdout)
+    end
+
+    def search(opts)
+      opts = prepare_opts(opts)
+      orig_offset = opts[:params][:offset]
+      resp = paginate(opts)
+      resp[:items] = resp[:items][0...(opts[:params][:limit] - orig_offset)]
+      block_given? ? yield(resp) : resp
+    end
+
+    def show(uuid)
+      query(path: "view/#{uuid}", method: :get)
+    end
+
+    def types
+      %w(records mediarecords recordsets publishers)
+    end
+
+    def count(opts = {})
+      opts = { type: "record", params: {} }.merge(opts)
+      res = query(path: "summary/count/#{opts[:type]}s/", params: opts[:params])
+      res ? res[:itemCount] : nil
+    end
+
+    def fields(type = "records")
+      type = normalize_type(type)
+      query(path: "meta/fields/#{type}", method: :get)
     end
 
     private
@@ -62,6 +85,47 @@ module IdigbioClient
         h[sym] = h.delete(k)
         symbolize(h[sym]) if h[sym].is_a? Hash
       end
+    end
+
+    def paginate(opts)
+      items = []
+      params = opts[:params]
+      while params[:items_to_go].nil? || params[:items_to_go] > 0
+        resp = query(opts)
+        items += resp[:items] if resp[:itemCount] > 0
+        adjust_params(resp, params)
+      end
+      resp[:items] = items
+      resp
+    end
+
+    def prepare_opts(opts)
+      symbolize(opts)
+      opts = { type: :records, method: :post, params: {} }.merge(opts)
+      opts[:params] = { rq: {}, limit: DEFAULT_LIMIT, offset: 0, fields: [],
+                        fields_exclude: [], sort: [] }.merge(opts[:params])
+      opts[:path] = prepare_path(opts[:type])
+      opts
+    end
+
+    def prepare_path(type)
+      type = normalize_type(type)
+      type.gsub!("mediarecords", "media")
+      "search/#{type}/"
+    end
+
+    def adjust_params(resp, params)
+      logger_write(resp, params)
+      params[:items_to_go] ||=
+        [MAX_LIMIT, resp[:itemCount], params[:limit]].min - params[:offset]
+      params[:offset] += resp[:items].size
+      params[:items_to_go] -= resp[:items].size
+    end
+
+    def logger_write(resp, params)
+      s = params[:offset] + 1
+      e = [params[:offset] + resp[:items].size, params[:limit]].min
+      logger.info("Processed items #{s}-#{e} out of #{params[:limit]}")
     end
   end
 end
--- lib/idigbio_client/show.rb
+++
@@ -1,8 +0,0 @@
-# Ruby wrapper for iDigBio API
-module IdigbioClient
-  class << self
-    def show(uuid)
-      query(path: "view/#{uuid}", method: :get)
-    end
-  end
-end
